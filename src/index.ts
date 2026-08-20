5a707975293e2fe42bc47da12450105e23119f73
Harold Barraza <harbar.kltgv@gmail.com>
2026-08-20 14:53:38 -0500

    swagger

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import readline from 'readline/promises';
 import { stdin as input, stdout as output } from 'process';
 
 const rl = readline.createInterface({ input, output });
-// 🚫 No eliminar las líneas de arriba ⬆️
 
-// ✍️ Escribe tu código aquí 👇
-
-
-// 🚫 No eliminar las líneas de abajo ⬇️
+console.log("hola mundo")
 rl.close();
+
