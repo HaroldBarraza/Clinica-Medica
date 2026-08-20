1fbb862747f95d878c4e85b944dc9f80c0799d9d
Harold Barraza <harbar.kltgv@gmail.com>
2026-08-20 14:45:36 -0500

    actulizacion

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
+const rl = readline.createInterface({ input, output });
+// 🚫 No eliminar las líneas de arriba ⬆️
+
+// ✍️ Escribe tu código aquí 👇
+
+
+// 🚫 No eliminar las líneas de abajo ⬇️
+rl.close();
