69d42b3242ac5fc5baa6acfbf75e34561a15b4a1
Harold Barraza <harbar.kltgv@gmail.com>
2026-08-21 14:52:41 -0500

    feature/host corregido

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,12 +1,16 @@
 
 import swaggerAutogen from "swagger-autogen"
+
+
 const doc = {
     info:{
         title: "API de Clinica",
         description: "Documentacion generada por autogen",
         version: "1.0.0"
     },
-    host: "localhost:3000"
+  host: "c87k99jw-3000.brs.devtunnels.ms",
+  basePath: "/",
+  schemes: ["https"],
 };
 
 const outputFile = "../swagger-output.json"
